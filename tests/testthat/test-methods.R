1c2403ac146c20255633be487856639e3a07640e
Dan Jordan <dandermotj@gmail.com>
2019-05-14 21:55:24 +0100

    Tests for `fit` including normal, binomial and bernoulli

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -7,3 +7,13 @@ test_that("pmf works", {
   expect_equal(pmf(N, 0), pdf(N, 0))
   expect_equal(pmf(B, 0), pdf(B, 0))
 })
+
+test_that("fit works", {
+  N <- normal()
+  B <- binomial(10)
+
+  expect_equal(normal(1, 0), fit(N, c(1, 1)))
+  expect_equal(bernoulli(), fit(bernoulli(), c(0, 1)))
+  expect_equal(binomial(10, 0.5), fit(B, c(0, 1)))
+
+})
