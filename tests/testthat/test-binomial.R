7610c7bd7f2390e75531895acc5ad2d0c53c6c9c
Dan Jordan <dandermotj@gmail.com>
2019-06-13 23:10:21 +0100

    Fix broken tests from CamelCase rename

--- tests/testthat/test-binomial.R
+++ tests/testthat/test-binomial.R
@@ -1,33 +1,33 @@
 context("test-binomial")
 
-test_that("fit.binomial works correctly", {
+test_that("fit.Binomial works correctly", {
 
   # only one trial
-  expect_equal(fit(binomial(1), c(0,1)), binomial(1, 0.5))
+  expect_equal(fit(Binomial(1), c(0,1)), Binomial(1, 0.5))
 
   # many trials
   x <- c(0, rep(100, 99))
-  expect_equal(fit(binomial(100), x), binomial(100, 0.99))
+  expect_equal(fit(Binomial(100), x), Binomial(100, 0.99))
 
   # raises error when data has negative counts
-  expect_error(fit(binomial(1), -1))
+  expect_error(fit(Binomial(1), -1))
 
-  # raises error when data has counts greater than binomial's size
-  expect_error(fit(binomial(1), 2))
+  # raises error when data has counts greater than Binomial's size
+  expect_error(fit(Binomial(1), 2))
 })
 
-test_that("suff_stat.binomial works correctly", {
+test_that("suff_stat.Binomial works correctly", {
 
   ss_1 <- list(successes = 1, experiments = 2, trials = 1)
-  expect_equal(suff_stat(binomial(1), c(0,1)), ss_1)
+  expect_equal(suff_stat(Binomial(1), c(0,1)), ss_1)
 
   ss_2 <- list(successes = 9, experiments = 3, trials = 5)
-  expect_equal(suff_stat(binomial(5), c(3,3,3)), ss_2)
+  expect_equal(suff_stat(Binomial(5), c(3,3,3)), ss_2)
 })
 
-test_that("likelihood.binomial and log_likelihood.binomial work correctly", {
+test_that("likelihood.Binomial and log_likelihood.Binomial work correctly", {
 
-  b <- binomial(size = 10, p = 0.1)
+  b <- Binomial(size = 10, p = 0.1)
   x <- c(1, 1, 0)
 
   expect_equal(likelihood(b, 1), dbinom(1, 10, 0.1))
--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -2,7 +2,7 @@ context("test-methods")
 
 test_that("pmf works", {
   N <- normal()
-  B <- bernoulli()
+  B <- Bernoulli()
 
   expect_equal(pmf(N, 0), pdf(N, 0))
   expect_equal(pmf(B, 0), pdf(B, 0))
@@ -10,11 +10,11 @@ test_that("pmf works", {
 
 test_that("fit works", {
   N <- normal()
-  bern <- bernoulli()
-  B <- binomial(10)
+  bern <- Bernoulli()
+  B <- Binomial(10)
 
   expect_equal(normal(1, 0), fit(N, c(1, 1)))
-  expect_equal(bernoulli(), fit(bern, c(0, 1)))
-  expect_equal(binomial(10, 0.5), fit(B, c(5,5)))
+  expect_equal(Bernoulli(), fit(bern, c(0, 1)))
+  expect_equal(Binomial(10, 0.5), fit(B, c(5,5)))
 
 })
