55b1a8f3fd62a293a984944b880d0377f568a189
Alessandro Gasparini <alessandro.gasparini@ki.se>
2019-11-07 16:15:03 +0100

    Started implementation of the Erlang distribution

---
+++ R/Erlang.R
@@ -0,0 +1,154 @@
+#' Create an Erlang distribution
+#'
+#' The Erlang distribution is a two-parameter family of continuous probability
+#' distributions with support \eqn{x \in [0,\infty)}.
+#' The two parameters are a positive integer shape parameter \eqn{k} and a
+#' positive real rate parameter \eqn{\lambda}.
+#' The Erlang distribution with shape parameter \eqn{k = 1} simplifies to the
+#' exponential distribution, and it is a special case of the gamma distribution.
+#' It corresponds to a sum of \eqn{k} independent exponential variables with mean
+#' \eqn{1 / \lambda} each.
+#'
+#' @param k The shape parameter. Can be any positive integer number.
+#' @param lambda The rate parameter. Can be any positive number.
+#' @return An `Erlang` object.
+#' @export
+#' @family continuous distributions
+#'
+#' @examples
+#'
+#' set.seed(27)
+#'
+#' X <- Erlang(5, 2)
+#' X
+#'
+#' random(X, 10)
+#'
+#' pdf(X, 2)
+#' log_pdf(X, 2)
+#'
+#' cdf(X, 4)
+#' quantile(X, 0.7)
+#'
+#' cdf(X, quantile(X, 0.7))
+#' quantile(X, cdf(X, 7))
+Erlang <- function(k, lambda) {
+  d <- list(k = k, lambda = lambda)
+  class(d) <- c("Erlang", "distribution")
+  d
+}
+
+#' @export
+print.Erlang <- function(x, ...) {
+  cat(glue("Erlang distribution (k = {x$k}, lambda = {x$lambda})\n"))
+}
+
+#' Draw a random sample from an Erlang distribution
+#'
+#' @inherit Erlang examples
+#'
+#' @param d An `Erlang` object created by a call to [Erlang()].
+#' @param n The number of samples to draw. Defaults to `1L`.
+#' @param ... Unused. Unevaluated arguments will generate a warning to
+#'   catch mispellings or other possible errors.
+#'
+#' @return A numeric vector of length `n`.
+#' @export
+#'
+random.Erlang <- function(d, n = 1L, ...) {
+  replicate(n = n, expr = {
+    -1 / d$lambda * log(prod(runif(n = d$k)))
+  })
+}
+
+#' Evaluate the probability mass function of an Erlang distribution
+#'
+#' @inherit Erlang examples
+#' @inheritParams random.Erlang
+#'
+#' @param x A vector of elements whose probabilities you would like to
+#'   determine given the distribution `d`.
+#' @param ... Unused. Unevaluated arguments will generate a warning to
+#'   catch mispellings or other possible errors.
+#'
+#' @return A vector of probabilities, one for each element of `x`.
+#' @export
+#'
+pdf.Erlang <- function(d, x, ...) {
+  (d$lambda^d$k) * (x^(d$k - 1)) * exp(-d$lambda * x) / factorial(d$k - 1)
+}
+
+#' @rdname pdf.Gamma
+#' @export
+#'
+log_pdf.Gamma <- function(d, x, ...) {
+  stop("`log_pdf` is not implemented for the Erlang distribution yet")
+}
+
+#' Evaluate the cumulative distribution function of an Erlang distribution
+#'
+#' @inherit Erlang examples
+#' @inheritParams random.Erlang
+#'
+#' @param x A vector of elements whose cumulative probabilities you would
+#'   like to determine given the distribution `d`.
+#' @param ... Unused. Unevaluated arguments will generate a warning to
+#'   catch mispellings or other possible errors.
+#'
+#' @return A vector of probabilities, one for each element of `x`.
+#' @export
+#'
+cdf.Erlang <- function(d, x, ...) {
+  internal <- Vectorize(FUN = function(d, x, ...) {
+    summation <- vector(mode = "numeric", length = d$k)
+    n <- 0:(d$k - 1)
+    summation <- 1 / factorial(n) * exp(-d$lambda * x) * (d$lambda * x)^n
+    1 - sum(summation)
+  }, vectorize.args = "x")
+  internal(d = d, x = x, ...)
+}
+
+#' Determine quantiles of an Erlang distribution
+#'
+#' `quantile()` is the inverse of `cdf()`.
+#'
+#' @inherit Erlang examples
+#' @inheritParams random.Erlang
+#'
+#' @param p A vector of probabilites.
+#' @param ... Unused. Unevaluated arguments will generate a warning to
+#'   catch mispellings or other possible errors.
+#'
+#' @return A vector of quantiles, one for each element of `p`.
+#' @export
+#'
+quantile.Erlang <- function(d, p, ...) {
+  stop("`quantile` is not implemented for the Erlang distribution yet")
+}
+
+#' Fit a Erlang distribution to data
+#'
+#' @param d A `Erlang` object created by a call to [Erlang()].
+#' @param x A vector to fit the Erlang distribution to.
+#' @param ... Unused.
+#'
+#' @return a `Erlang` object
+#' @export
+fit_mle.Erlang <- function(d, x, ...) {
+  # ss <- suff_stat(d, x, ...)
+  stop("`fit_mle` is not implemented for the Erlang distribution yet")
+}
+
+#' Compute the sufficient statistics for an Erlang distribution from data
+#'
+#'   - `sum`: The sum of the data.
+#'   - `log_sum`: The log of the sum of the data.
+#'   - `samples`: The number of samples in the data.
+#'
+#' @inherit fit_mle.Erlang
+#' @export
+suff_stat.Erlang <- function(d, x, ...) {
+  # if (any(x < 0)) stop("`x` must only contain positive real numbers")
+  # list(sum = sum(x), log_sum = sum(log(x)), samples = length(x))
+  stop("`suff_stat` is not implemented for the Erlang distribution yet")
+}
