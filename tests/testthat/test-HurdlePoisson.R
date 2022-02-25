b191e386e44b9ea85ac05395f4772b00c72aaa6c
Achim Zeileis <Achim.Zeileis@uibk.ac.at>
2022-02-25 01:40:17 +0100

    added tests for new zero-augmented distributions

---
+++ tests/testthat/test-HurdlePoisson.R
@@ -0,0 +1,130 @@
+context("test-HurdlePoisson")
+
+test_that("print.HurdlePoisson works", {
+  expect_output(print(HurdlePoisson(1, 0.7)), regexp = "HurdlePoisson distribution")
+})
+
+test_that("random.HurdlePoisson work correctly", {
+  p <- HurdlePoisson(1, 0.7)
+
+  expect_length(random(p), 1)
+  expect_length(random(p, 100), 100)
+  expect_length(random(p, 0), 0)
+  expect_error(random(p, -2))
+})
+
+test_that("pdf.HurdlePoisson work correctly", {
+  p <- HurdlePoisson(1, 0.7)
+
+  expect_equal(pdf(p, 0), dhpois(0, 1, 0.7))
+  expect_equal(pdf(p, 1), dhpois(1, 1, 0.7))
+  expect_equal(pdf(p, -12), 0)
+
+  expect_warning(pdf(p, 0.5))
+
+  expect_length(pdf(p, seq_len(0)), 0)
+  expect_length(pdf(p, seq_len(1)), 1)
+  expect_length(pdf(p, seq_len(10)), 10)
+})
+
+test_that("log_pdf.HurdlePoisson work correctly", {
+  p <- HurdlePoisson(1, 0.7)
+
+  expect_equal(log_pdf(p, 0), dhpois(0, 1, 0.7, log = TRUE))
+  expect_equal(log_pdf(p, 1), dhpois(1, 1, 0.7, log = TRUE))
+  expect_equal(log_pdf(p, -12), -Inf)
+
+  expect_warning(log_pdf(p, 0.5))
+
+  expect_length(log_pdf(p, seq_len(0)), 0)
+  expect_length(log_pdf(p, seq_len(1)), 1)
+  expect_length(log_pdf(p, seq_len(10)), 10)
+})
+
+test_that("cdf.HurdlePoisson work correctly", {
+  p <- HurdlePoisson(1, 0.7)
+
+  expect_equal(cdf(p, 0), phpois(0, 1, 0.7))
+  expect_equal(cdf(p, 1), phpois(1, 1, 0.7))
+
+
+  expect_length(cdf(p, seq_len(0)), 0)
+  expect_length(cdf(p, seq_len(1)), 1)
+  expect_length(cdf(p, seq_len(10)), 10)
+})
+
+test_that("quantile.HurdlePoisson work correctly", {
+  p <- HurdlePoisson(1, 0.7)
+
+  expect_equal(quantile(p, 0), 0)
+  expect_equal(quantile(p, 0.5), 1)
+
+
+  expect_length(quantile(p, seq_len(0)), 0)
+  expect_length(quantile(p, c(0, 1)), 2)
+})
+
+test_that("vectorization of a HurdlePoisson distribution work correctly", {
+  d <- HurdlePoisson(c(1, 2), 0.7)
+  d1 <- d[1]
+  d2 <- d[2]
+
+  ## moments
+  expect_equal(mean(d), c(mean(d1), mean(d2)))
+  expect_equal(variance(d), c(variance(d1), variance(d2)))
+  expect_equal(skewness(d), c(skewness(d1), skewness(d2)))
+  expect_equal(kurtosis(d), c(kurtosis(d1), kurtosis(d2)))
+
+  ## pdf, log_pdf, cdf
+  expect_equal(pdf(d, 0), c(pdf(d1, 0), pdf(d2, 0)))
+  expect_equal(log_pdf(d, 0), c(log_pdf(d1, 0), log_pdf(d2, 0)))
+  expect_equal(cdf(d, 0.5), c(cdf(d1, 0.5), cdf(d2, 0.5)))
+
+  ## quantile
+  expect_equal(quantile(d, 0.5), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(quantile(d, c(0.5, 0.5)), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(
+    quantile(d, c(0.1, 0.5, 0.9)),
+    rbind(quantile(d1, c(0.1, 0.5, 0.9)), quantile(d2, c(0.1, 0.5, 0.9)))
+  )
+
+  ## support
+  expect_equal(
+    support(d),
+    matrix(
+      c(support(d1)[1], support(d2)[1], support(d1)[2], support(d2)[2]),
+      ncol = 2, dimnames = list(names(d), c("min", "max"))
+    )
+  )
+  expect_true(is.numeric(support(d1)))
+  expect_true(is.numeric(support(d1, drop = FALSE)))
+  expect_null(dim(support(d1)))
+  expect_equal(dim(support(d1, drop = FALSE)), c(1L, 2L))
+})
+
+test_that("named return values for HurdlePoisson distribution work correctly", {
+  d <- HurdlePoisson(c(5, 10), 0.75)
+  names(d) <- LETTERS[1:length(d)]
+
+  expect_equal(names(mean(d)), LETTERS[1:length(d)])
+  expect_equal(names(variance(d)), LETTERS[1:length(d)])
+  expect_equal(names(skewness(d)), LETTERS[1:length(d)])
+  expect_equal(names(kurtosis(d)), LETTERS[1:length(d)])
+  expect_equal(names(random(d, 1)), LETTERS[1:length(d)])
+  expect_equal(rownames(random(d, 3)), LETTERS[1:length(d)])
+  expect_equal(names(pdf(d, 5)), LETTERS[1:length(d)])
+  expect_equal(names(pdf(d, c(5, 7))), LETTERS[1:length(d)])
+  expect_equal(rownames(pdf(d, c(5, 7, 9))), LETTERS[1:length(d)])
+  expect_equal(names(log_pdf(d, 5)), LETTERS[1:length(d)])
+  expect_equal(names(log_pdf(d, c(5, 7))), LETTERS[1:length(d)])
+  expect_equal(rownames(log_pdf(d, c(5, 7, 9))), LETTERS[1:length(d)])
+  expect_equal(names(cdf(d, 5)), LETTERS[1:length(d)])
+  expect_equal(names(cdf(d, c(5, 7))), LETTERS[1:length(d)])
+  expect_equal(rownames(cdf(d, c(5, 7, 9))), LETTERS[1:length(d)])
+  expect_equal(names(quantile(d, 0.5)), LETTERS[1:length(d)])
+  expect_equal(names(quantile(d, c(0.5, 0.7))), LETTERS[1:length(d)])
+  expect_equal(rownames(quantile(d, c(0.5, 0.7, 0.9))), LETTERS[1:length(d)])
+  expect_equal(names(support(d[1])), c("min", "max"))
+  expect_equal(colnames(support(d)), c("min", "max"))
+  expect_equal(rownames(support(d)), LETTERS[1:length(d)])
+})
---
+++ tests/testthat/test-ZIPoisson.R
@@ -0,0 +1,130 @@
+context("test-ZIPoisson")
+
+test_that("print.ZIPoisson works", {
+  expect_output(print(ZIPoisson(1, 0.2)), regexp = "ZIPoisson distribution")
+})
+
+test_that("random.ZIPoisson work correctly", {
+  p <- ZIPoisson(1, 0.2)
+
+  expect_length(random(p), 1)
+  expect_length(random(p, 100), 100)
+  expect_length(random(p, 0), 0)
+  expect_error(random(p, -2))
+})
+
+test_that("pdf.ZIPoisson work correctly", {
+  p <- ZIPoisson(1, 0.2)
+
+  expect_equal(pdf(p, 0), 0.8 / exp(1) + 0.2)
+  expect_equal(pdf(p, 1), 0.8 / exp(1))
+  expect_equal(pdf(p, -12), 0)
+
+  expect_warning(pdf(p, 0.5))
+
+  expect_length(pdf(p, seq_len(0)), 0)
+  expect_length(pdf(p, seq_len(1)), 1)
+  expect_length(pdf(p, seq_len(10)), 10)
+})
+
+test_that("log_pdf.ZIPoisson work correctly", {
+  p <- ZIPoisson(1, 0.2)
+
+  expect_equal(log_pdf(p, 0), log(0.8 / exp(1) + 0.2))
+  expect_equal(log_pdf(p, 1), log(0.8) - 1)
+  expect_equal(log_pdf(p, -12), -Inf)
+
+  expect_warning(log_pdf(p, 0.5))
+
+  expect_length(log_pdf(p, seq_len(0)), 0)
+  expect_length(log_pdf(p, seq_len(1)), 1)
+  expect_length(log_pdf(p, seq_len(10)), 10)
+})
+
+test_that("cdf.ZIPoisson work correctly", {
+  p <- ZIPoisson(1, 0.2)
+
+  expect_equal(cdf(p, 0), 0.8 / exp(1) + 0.2)
+  expect_equal(cdf(p, 1), 0.8 / exp(1) * 2 + 0.2)
+
+
+  expect_length(cdf(p, seq_len(0)), 0)
+  expect_length(cdf(p, seq_len(1)), 1)
+  expect_length(cdf(p, seq_len(10)), 10)
+})
+
+test_that("quantile.ZIPoisson work correctly", {
+  p <- ZIPoisson(1, 0.2)
+
+  expect_equal(quantile(p, 0), 0)
+  expect_equal(quantile(p, 0.5), 1)
+
+
+  expect_length(quantile(p, seq_len(0)), 0)
+  expect_length(quantile(p, c(0, 1)), 2)
+})
+
+test_that("vectorization of a ZIPoisson distribution work correctly", {
+  d <- ZIPoisson(c(1, 2), 0.2)
+  d1 <- d[1]
+  d2 <- d[2]
+
+  ## moments
+  expect_equal(mean(d), c(mean(d1), mean(d2)))
+  expect_equal(variance(d), c(variance(d1), variance(d2)))
+  expect_equal(skewness(d), c(skewness(d1), skewness(d2)))
+  expect_equal(kurtosis(d), c(kurtosis(d1), kurtosis(d2)))
+
+  ## pdf, log_pdf, cdf
+  expect_equal(pdf(d, 0), c(pdf(d1, 0), pdf(d2, 0)))
+  expect_equal(log_pdf(d, 0), c(log_pdf(d1, 0), log_pdf(d2, 0)))
+  expect_equal(cdf(d, 0.5), c(cdf(d1, 0.5), cdf(d2, 0.5)))
+
+  ## quantile
+  expect_equal(quantile(d, 0.5), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(quantile(d, c(0.5, 0.5)), c(quantile(d1, 0.5), quantile(d2, 0.5)))
+  expect_equal(
+    quantile(d, c(0.1, 0.5, 0.9)),
+    rbind(quantile(d1, c(0.1, 0.5, 0.9)), quantile(d2, c(0.1, 0.5, 0.9)))
+  )
+
+  ## support
+  expect_equal(
+    support(d),
+    matrix(
+      c(support(d1)[1], support(d2)[1], support(d1)[2], support(d2)[2]),
+      ncol = 2, dimnames = list(names(d), c("min", "max"))
+    )
+  )
+  expect_true(is.numeric(support(d1)))
+  expect_true(is.numeric(support(d1, drop = FALSE)))
+  expect_null(dim(support(d1)))
+  expect_equal(dim(support(d1, drop = FALSE)), c(1L, 2L))
+})
+
+test_that("named return values for ZIPoisson distribution work correctly", {
+  d <- ZIPoisson(c(5, 10), 0.25)
+  names(d) <- LETTERS[1:length(d)]
+
+  expect_equal(names(mean(d)), LETTERS[1:length(d)])
+  expect_equal(names(variance(d)), LETTERS[1:length(d)])
+  expect_equal(names(skewness(d)), LETTERS[1:length(d)])
+  expect_equal(names(kurtosis(d)), LETTERS[1:length(d)])
+  expect_equal(names(random(d, 1)), LETTERS[1:length(d)])
+  expect_equal(rownames(random(d, 3)), LETTERS[1:length(d)])
+  expect_equal(names(pdf(d, 5)), LETTERS[1:length(d)])
+  expect_equal(names(pdf(d, c(5, 7))), LETTERS[1:length(d)])
+  expect_equal(rownames(pdf(d, c(5, 7, 9))), LETTERS[1:length(d)])
+  expect_equal(names(log_pdf(d, 5)), LETTERS[1:length(d)])
+  expect_equal(names(log_pdf(d, c(5, 7))), LETTERS[1:length(d)])
+  expect_equal(rownames(log_pdf(d, c(5, 7, 9))), LETTERS[1:length(d)])
+  expect_equal(names(cdf(d, 5)), LETTERS[1:length(d)])
+  expect_equal(names(cdf(d, c(5, 7))), LETTERS[1:length(d)])
+  expect_equal(rownames(cdf(d, c(5, 7, 9))), LETTERS[1:length(d)])
+  expect_equal(names(quantile(d, 0.5)), LETTERS[1:length(d)])
+  expect_equal(names(quantile(d, c(0.5, 0.7))), LETTERS[1:length(d)])
+  expect_equal(rownames(quantile(d, c(0.5, 0.7, 0.9))), LETTERS[1:length(d)])
+  expect_equal(names(support(d[1])), c("min", "max"))
+  expect_equal(colnames(support(d)), c("min", "max"))
+  expect_equal(rownames(support(d)), LETTERS[1:length(d)])
+})
